d633c494f2ca3ca26ccb5379335d27976a116dcb
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2024-02-24 16:28:06 +0700

    55. Membuat Logout Action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -36,4 +36,13 @@ class UserControllerTest extends TestCase
             "password" => "wrong"
         ])->assertSeeText("User or password is wrong");
     }
+
+    public function testLogout()
+    {
+        $this->withSession([
+            "user" => "dsyafaatul"
+        ])->post("/logout")
+        ->assertRedirect("/")
+        ->assertSessionMissing("user");
+    }
 }
